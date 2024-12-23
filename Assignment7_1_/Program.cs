6b31f3da60c95cc4dfe1437a5ce3f3f8d7fcac1e
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2024-12-23 18:46:24 -0500

    Asst 7.1

---
+++ Assignment7_1_/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Assignment7_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assignment7.1.1");
+            Console.WriteLine("===============\n");
+
+            Console.WriteLine("Assignment7.1.2");
+            Console.WriteLine("===============\n");
+
+            string result1 = MergeStrings("abc", "pqr");
+            Console.WriteLine($"Output 1: {result1}");
+
+            string result2 = MergeStrings("ab", "pqrs");
+            Console.WriteLine($"Output 2: {result2}");
+        }
+
+        static string MergeStrings(string word1, string word2)
+        {
+            string mergedString = "";
+            int maxLength = Math.Max(word1.Length, word2.Length);
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < word1.Length)
+                    mergedString += word1[i];
+                if (i < word2.Length)
+                    mergedString += word2[i];
+            }
+
+            return mergedString;
+        }
+    }
+}
